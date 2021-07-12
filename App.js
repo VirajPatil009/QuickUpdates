b7394125bdded447cfb64e5d28a613f9982f2ac1
Viraj <virajpatil009@gmail.com>
2021-07-12 15:28:15 +0530

    updated tabs

--- App.js
+++ App.js
@@ -3,48 +3,12 @@ import React from "react";
 
 import Home from "./screens/home";
 import Content from "./screens/addpost";
-import Tab1 from "./tabs/Tab1";
-import Tab2 from "./tabs/Tab2";
-import Tab3 from "./tabs/Tab3";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-// import { createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs'
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { useColorScheme, AppearanceProvider } from "react-native-appearance";
+import { AppearanceProvider } from "react-native-appearance";
 
-const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
-const TopTab = createMaterialTopTabNavigator();
-// const BottomTab = createMaterialBottomTabNavigator();
-const linking = {
-  prefixes: ["recipes://"],
-  config: {
-    screens: {
-      Home: "feed/:title",
-      Content: "detail/:foodName",
-
-      TopTabs: {
-        path: "top_tabs",
-        screens: {
-          Tab1: {
-            path: "tTab1",
-            exact: true,
-          },
-          Tab2: {
-            path: "tTab2",
-            exact: true,
-          },
-          Tab3: {
-            path: "tTab3",
-            exact: true,
-          },
-        },
-      },
-    },
-  },
-};
 
 export default function App() {
   createHomeStack = () => (
@@ -61,9 +25,7 @@ export default function App() {
   return (
     <>
       <AppearanceProvider>
-        <NavigationContainer linking={linking}>
-          {createHomeStack()}
-        </NavigationContainer>
+        <NavigationContainer>{createHomeStack()}</NavigationContainer>
       </AppearanceProvider>
     </>
   );
